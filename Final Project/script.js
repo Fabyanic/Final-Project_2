bdf7d27c83c97df99d50d328f124d8e16d6a43ce
fabyanic <mmfab8306@aol.com>
2016-10-31 13:30:22 -0400

    Add constats to script. Worked on recipe format webpage

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,8 +1,24 @@
 /**
  * Created by mmfab on 10/30/2016.
  */
+const browniesButter= 0.5;
+const browniescupOfWhiteSugar=1;
+const eggsbrownieseggs=2;
+const browniesvillina= 1;
+const browniesCocoaPowder =1/3;
+const browniesbakepowder =0.25;
+const browniessalt = 0.25;
 
+function testthing() {
+
+    document.getElementById("test").innerHTML=
+
+
+}
 function ajustment() {
+
+
+
 
 
 
@@ -11,9 +27,5 @@ function ajustment() {
 }
 
 function DisplayBrownies() {
-
-
-
-
 
 }
