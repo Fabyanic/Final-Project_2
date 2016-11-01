84edb83b6d3ccc382ce0bb74ab0ce71db5bb1b2c
fabyanic <mmfab8306@aol.com>
2016-11-01 14:52:52 -0400

    Make input txtbox for custom setting on brownies page with id of timestxt. Get input from timestxt in javascirpt. Take the input and make it go through the Calculation function with brownies constants. Changed name for the brownie constants for ingreints. Set up table for the div outputs for brownie page for changing the amounts with custom setting.

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,14 +1,14 @@
 /**
  * Created by mmfab on 10/30/2016.
  */
-const browniesButter= 0.5;
-const browniescupOfWhiteSugar=1;
-const eggsbrownieseggs=2;
-const browniesvillina= 1;
-const browniesCocoaPowder =1/3;
-const browniesbakepowder =0.25;
-const browniessalt = 0.25;
-const test=2.23;
+const amountOfButterForBrownies= 0.5;
+const amountOfSugarForBrownies=1;
+const amountOfEggsForBrownies=2;
+const amountOfVanillaExtractForBrownies= 1;
+const amountOfCocoaPowderForBrownies =1/3;
+const amountOfFlourForBrownies =0.5;
+const amountOfSaltForBrownies = 0.25;
+const amountOfBakingPowderForBrownies=2.23;
 
 
 
@@ -43,30 +43,40 @@ function Multiply(amount,times) {
 
 }
 
-function calculations() {
-    var totalamount =Multiply();
+function calculations(times,constant) {
+    var output="";
+    var totalamount =Multiply(times,constant);
     if( totalamount%1!=0){
 
          var  decimal =remainder(totalamount);
         var bottomOfFraction=findFraction( decimal);
+        output= totalamount.toFixed(0)+" "+"1/"+bottomOfFraction;
+
+    }else {
+        output= totalamount.toFixed(0);
     }
+    return output;
 
 
 
 }
 
-function ajustment() {
+
+function Browniesfortxt() {
 
 
 
+    var times= parseFloat(document.getElementById("timestxt").value);
+    var outputPart1Butter= calculations(times,amountOfButterForBrownies);
+    var outputPart1Sugar= calculations(times,amountOfSugarForBrownies);
+    var outputPart1Eggs= calculations(times,amountOfEggsForBrownies);
+    var outputPart1VaillaExtract= calculations(times,amountOfVanillaExtractForBrownies);
+    var outputPart1CocoaPowder= calculations(times,amountOfCocoaPowderForBrownies);
+    var outputPart1Flour= calculations(times, amountOfFlourForBrownies);
+    var outputPart1Salt= calculations(times,amountOfSaltForBrownies);
+    var outputPart1BakingPowder= calculations(times,amountOfBakingPowderForBrownies);
 
 
-
-
-
-}
-
-function DisplayBrownies() {
 
 
 
