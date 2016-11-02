818d1e247c332bbc9468ed3ed4c7d5ecde39744a
fabyanic <mmfab8306@aol.com>
2016-11-02 12:06:31 -0400

    Made findfraction function work. Made gCF function work. Made Remainder function. Connected to brownie page. Changed format for starting recipe. input div elements for table that outputs the changed amounts for recipe.

--- Final Project/script.js
+++ Final Project/script.js
@@ -1,62 +1,145 @@
 /**
  * Created by mmfab on 10/30/2016.
  */
-const amountOfButterForBrownies= 0.5;
-const amountOfSugarForBrownies=1;
-const amountOfEggsForBrownies=2;
-const amountOfVanillaExtractForBrownies= 1;
-const amountOfCocoaPowderForBrownies =1/3;
-const amountOfFlourForBrownies =0.5;
+const amountOfButterForBrownies = 0.5;
+const amountOfSugarForBrownies = 1;
+const amountOfEggsForBrownies = 2;
+const amountOfVanillaExtractForBrownies = 1;
+const amountOfCocoaPowderForBrownies = 1 / 3;
+const amountOfFlourForBrownies = 0.5;
 const amountOfSaltForBrownies = 0.25;
-const amountOfBakingPowderForBrownies=2.23;
-
-
+const amountOfBakingPowderForBrownies = 0.25;
 
 
 function remainder(amount) {
-    var decimal= amount%1;
+    var decimal = amount % 1;
 
-
-
+return decimal;
 }
 
 function findFraction(decimal) {
+    var output;
+    var fraction = decimal;
+    var deminator;
+    var wholeNumber=0;
+    var timeOfLoops=0;
+    if( decimal>0.32&&decimal<0.34){
+        wholeNumber=1;
+        deminator=3;
+        output= wholeNumber+"/"+deminator;
+        return output;
 
-    var fraction=1;
-    var deminator=1;
-    while ( decimal!= fraction){
-        deminator++;
-        var numinrator=1;
-        fraction= numinrator/deminator;
 
     }
-    return deminator;
+    if (decimal>0.65&&decimal<0.67){
+
+        wholeNumber=2;
+        deminator=3;
+        output= wholeNumber+"/"+deminator;
+        return output;
+    }
+
+
+
+
+
+        do {
+
+            timeOfLoops++;
+            wholeNumber= decimal*10;
+            decimal=wholeNumber;
+            if(wholeNumber%1==0){
+                deminator=Math.pow(10,timeOfLoops)
+
+            }
+            fraction2= wholeNumber/deminator;
+
+        }while( fraction2!= fraction)
+
+    var gCF=findGCF(wholeNumber,deminator);
+    var topOfFraction;
+    var bottomOfFraction;
+    topOfFraction= wholeNumber/gCF;
+    bottomOfFraction=deminator/gCF;
+
+
+    output= topOfFraction+"/"+ bottomOfFraction;
+    return output;
+
+
+
+
+
+
 
 }
-function Multiply(amount,times) {
-    var totalAmountneeded= amount*times;
+
+function findGCF(wholenumber,deminator) {
+
+    var quient=0.5;
+    var remainder;
+    var muiltiply;
+
+    while (quient%1!=0|| remainder!=0){
+
+        quient= deminator/wholenumber;
+        quient= Math.floor(quient);
+        muiltiply= wholenumber*quient;
+        remainder= deminator-muiltiply;
+        if(remainder!= 0){
+            deminator=wholenumber;
+            wholenumber= remainder;
+        }
+
+
+
+
+
+    }
+
+    return wholenumber;
+
+
+
+
+
+
+
+
+
+
+}
+function Multiply(amount, times) {
+    var totalAmountneeded = amount * times;
 
     return totalAmountneeded;
 
 
-
-
 }
 
-function calculations(times,constant) {
-    var output="";
-    var totalamount =Multiply(times,constant);
-    if( totalamount%1!=0){
+function calculations(times, constant) {
+    var output = "";
+    var totalamount = Multiply(times, constant);
+    if (totalamount % 1 != 0) {
 
-         var  decimal =remainder(totalamount);
-        var bottomOfFraction=findFraction( decimal);
-        output= totalamount.toFixed(0)+" "+"1/"+bottomOfFraction;
+        var decimal = remainder(totalamount);
+        var output1 = findFraction(decimal,times);
+        totalamount=Math.floor(totalamount);
+        if (totalamount!=0){
 
-    }else {
-        output= totalamount.toFixed(0);
+        output= totalamount.toFixed(0)+" " +output1;
+        return output;
+        }else {
+            output= output1;
+
+        return output;}
+
+
+
     }
+    Math.floor(totalamount);
+    output= totalamount.toFixed(0);
     return output;
-
 
 
 }
@@ -65,20 +148,27 @@ function calculations(times,constant) {
 function Browniesfortxt() {
 
 
-
-    var times= parseFloat(document.getElementById("timestxt").value);
-    var outputPart1Butter= calculations(times,amountOfButterForBrownies);
-    var outputPart1Sugar= calculations(times,amountOfSugarForBrownies);
-    var outputPart1Eggs= calculations(times,amountOfEggsForBrownies);
-    var outputPart1VaillaExtract= calculations(times,amountOfVanillaExtractForBrownies);
-    var outputPart1CocoaPowder= calculations(times,amountOfCocoaPowderForBrownies);
-    var outputPart1Flour= calculations(times, amountOfFlourForBrownies);
-    var outputPart1Salt= calculations(times,amountOfSaltForBrownies);
-    var outputPart1BakingPowder= calculations(times,amountOfBakingPowderForBrownies);
+    var times = parseFloat(document.getElementById("timestxt").value);
+    
 
 
+    var outputPart1Butter = calculations(times, amountOfButterForBrownies);
+    var outputPart1Sugar = calculations(times, amountOfSugarForBrownies);
+    var outputPart1Eggs = calculations(times, amountOfEggsForBrownies);
+    var outputPart1VaillaExtract = calculations(times, amountOfVanillaExtractForBrownies);
+    var outputPart1CocoaPowder = calculations(times, amountOfCocoaPowderForBrownies);
+    var outputPart1Flour = calculations(times, amountOfFlourForBrownies);
+    var outputPart1Salt = calculations(times, amountOfSaltForBrownies);
+    var outputPart1BakingPowder = calculations(times, amountOfBakingPowderForBrownies);
 
-
+    document.getElementById("browniesButter").innerHTML = outputPart1Butter + " cup butter";
+    document.getElementById("browniesWhiteSugar").innerHTML = outputPart1Sugar + " cup white sugar";
+    document.getElementById("browniesEggs").innerHTML = outputPart1Eggs + " eggs";
+    document.getElementById("browniesVanillaExtract").innerHTML = outputPart1VaillaExtract + " teaspoon Vanilla Extract";
+    document.getElementById("brownieCocoaPowder").innerHTML = outputPart1CocoaPowder + " cup Unsweetened cocoa powder";
+    document.getElementById("browniesFlour").innerHTML = outputPart1Flour + " cup all-purpose flour";
+    document.getElementById("browniesSalt").innerHTML = outputPart1Salt + " teaspoon Salt";
+    document.getElementById("browniesBakingPowder").innerHTML = outputPart1BakingPowder + " teaspoon baking powder";
 
 
 }
